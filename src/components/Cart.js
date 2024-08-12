e3263d79c68b17ce2a37a1ba05b6f26bed03c6f6
Lucifer <Lucifer@gmail.com>
2024-08-13 01:12:46 +0530

    naming of itemlist was wrong

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
-import ItemList from "./itemList";
+import ItemList from "./itemlist";
 import { selectTotalQuantity, clearCart } from "../utils/cartSlice";
 import Bill from "./Bill";
 import EmptyCart from "./EmptyCart";
